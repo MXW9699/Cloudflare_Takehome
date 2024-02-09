a383f1552daaf7078560796499f3eb5aef140b8a
MXW9699 <mitchell.wen@hotmail.com>
2024-02-09 14:17:42 -0500

    set up global error handler to the application

--- server/server.js
+++ server/server.js
@@ -4,10 +4,21 @@ const PORT = 3000;
 
 const app = express();
 
+/******************GLOBAL ERROR HANDLER********************************** */
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Error with express middleware',
+    status: 500,
+    message: { err: 'An error has occured' },
+  };
 
+  const customErr = Object.assign(defaultErr, err);
 
-
-
+  //display error in server
+  console.log(customErr.log);
+  //send the error message to the client
+  return res.status(customErr.status).json(customErr.message);
+});
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
